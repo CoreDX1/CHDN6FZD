67d9ba5f11404411f61d8ada4e9a0edb3d70975e
CoreDX1 <chrismaquis@gmail.com>
2022-02-16 15:37:36 -0300

    Pulido V1.20 || 15:37

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const galeria = [
     {
         id: "wal1",
+        class: "walC0",
         nombre: "Alchemist",
         imagen: "images/imagen_01.png",
         movilidad: "305",
@@ -10,6 +11,7 @@ const galeria = [
     },
     {
         id: "wal2",
+        class: "walC1",
         nombre: "Naga",
         imagen: "images/imagen_02.png",
         movilidad: "320",
@@ -19,6 +21,7 @@ const galeria = [
     },
     {
         id: "wal3",
+        class: "walC2",
         nombre: "Terrorblade",
         imagen: "images/imagen_03.png",
         movilidad: "315",
@@ -28,6 +31,7 @@ const galeria = [
     },
     {
         id: "wal4",
+        class: "walC3",
         nombre: "Luna",
         imagen: "images/imagen_04.png",
         movilidad: "325",
@@ -43,86 +47,39 @@ const menu = document.getElementById("menu");
 for ( menuBar of galeria ){
     let contenedorMenu = document.createElement("li");
     contenedorMenu.innerHTML = `
-    <a href="#" id="${menuBar.id}">${menuBar.nombre}</a>`
+    <a href="#" class="${menuBar.class}" id="${menuBar.id}">${menuBar.nombre}</a>`
     menu.appendChild(contenedorMenu)
 }
 
-const wal0 = document.getElementById("wal1")
-const wal1 = document.getElementById("wal2")
-const wal2 = document.getElementById("wal3")
-const wal3 = document.getElementById("wal4")
-const hero = document.getElementById("hero")
 
-const atributo_ = document.getElementById("atributo_")
-const atributo_num = document.getElementById("atributo_num")
-const velocidad_ = document.getElementById("velocidad_")
-const velocidad_num = document.getElementById("velocidad_num")
-const tipo_= document.getElementById("tipo_")
-const tipo_ataque= document.getElementById("tipo_ataque")
-
-
-let imagen_00 = galeria[0]["imagen"]
-let imagen_01 = galeria[1]["imagen"]
-let imagen_02 = galeria[2]["imagen"]
-let imagen_03 = galeria[3]["imagen"]
-
-let nombre_00 = galeria[0]["nombre"]
-let nombre_01 = galeria[1]["nombre"]
-let nombre_02 = galeria[2]["nombre"]
-let nombre_03 = galeria[3]["nombre"]
-
-let tipoAlche = galeria[0]["tipo"]
-let atributoAlche = galeria[0]["atributo"]
-let ataqueAlche = galeria[0]["ataque"]
-let velocidadAlche = galeria[0]["movilidad"]
-
-let tipoNaga = galeria[1]["tipo"]
-let atributoNaga = galeria[1]["atributo"]
-let ataqueNaga = galeria[1]["ataque"]
-let velocidadNaga = galeria[1]["movilidad"]
-
-let tipoTerro = galeria[2]["tipo"]
-let atributoTerro = galeria[2]["atributo"]
-let ataqueTerro = galeria[2]["ataque"]
-let velocidadTerro = galeria[2]["movilidad"]
-
-let tipoLuna = galeria[3]["tipo"]
-let atributoLuna = galeria[3]["atributo"]
-let ataqueLuna = galeria[3]["ataque"]
-let velocidadLuna = galeria[3]["movilidad"]
-
-wal0.addEventListener("click",function(){
-    header.style.backgroundImage = `url(${imagen_00})`;
-    hero.innerHTML = `${nombre_00}`;
-    atributo_num.innerHTML = `${atributoAlche}`
-    atributo_.innerHTML = `${tipoAlche}`
-    tipo_ataque.innerHTML = `${ataqueAlche}`
-    velocidad_num.innerHTML = `${velocidadAlche}`
-})
-
-wal1.addEventListener("click",function(){
-    header.style.backgroundImage = `url(${imagen_01})`;
-    hero.innerHTML = `${nombre_01}`;
-    atributo_num.innerHTML = `${atributoNaga}`
-    atributo_.innerHTML = `${tipoNaga}`
-    tipo_ataque.innerHTML = `${ataqueNaga}`
-    velocidad_num.innerHTML = `${velocidadNaga}`
-})
-
-wal2.addEventListener("click",function(){
-    header.style.backgroundImage = `url(${imagen_02})`;
-    hero.innerHTML = `${nombre_02}`;
-    atributo_num.innerHTML = `${atributoTerro}`
-    atributo_.innerHTML = `${tipoTerro}`
-    tipo_ataque.innerHTML = `${ataqueTerro}`
-    velocidad_num.innerHTML = `${velocidadTerro}`
-})
-
-wal3.addEventListener("click",function(){
-    header.style.backgroundImage = `url(${imagen_03})`;
-    hero.innerHTML = `${nombre_03}`;
-    atributo_num.innerHTML = `${atributoLuna}`
-    atributo_.innerHTML = `${tipoLuna}`
-    tipo_ataque.innerHTML = `${ataqueLuna}`
-    velocidad_num.innerHTML = `${velocidadLuna}`
+header.addEventListener("click", (e) => {
+    if(e.target.classList.contains("walC0")){ 
+        header.style.backgroundImage = `url(${galeria[0]["imagen"]})`;
+        hero.innerHTML = `${galeria[0]["nombre"]}`;
+        atributo_num.innerHTML = `${galeria[0]["atributo"]}`
+        atributo_.innerHTML = `${galeria[0]["tipo"]}`
+        tipo_ataque.innerHTML = `${galeria[0]["ataque"]}`
+        velocidad_num.innerHTML = `${galeria[0]["movilidad"]}`
+    }else if (e.target.classList.contains("walC1")){
+        header.style.backgroundImage = `url(${galeria[1]["imagen"]})`;
+        hero.innerHTML = `${galeria[1]["nombre"]}`;
+        atributo_num.innerHTML = `${galeria[1]["atributo"]}`
+        atributo_.innerHTML = `${galeria[1]["tipo"]}`
+        tipo_ataque.innerHTML = `${galeria[1]["ataque"]}`
+        velocidad_num.innerHTML = `${galeria[1]["movilidad"]}`
+    }else if (e.target.classList.contains("walC2")){
+        header.style.backgroundImage = `url(${galeria[2]["imagen"]})`;
+        hero.innerHTML = `${galeria[2]["nombre"]}`;
+        atributo_num.innerHTML = `${galeria[2]["atributo"]}`
+        atributo_.innerHTML = `${galeria[2]["tipo"]}`
+        tipo_ataque.innerHTML = `${galeria[2]["ataque"]}`
+        velocidad_num.innerHTML = `${galeria[2]["movilidad"]}`
+    }else if (e.target.classList.contains("walC3")){
+        header.style.backgroundImage = `url(${galeria[3]["imagen"]})`;
+        hero.innerHTML = `${galeria[3]["nombre"]}`;
+        atributo_num.innerHTML = `${galeria[3]["atributo"]}`
+        atributo_.innerHTML = `${galeria[3]["tipo"]}`
+        tipo_ataque.innerHTML = `${galeria[3]["ataque"]}`
+        velocidad_num.innerHTML = `${galeria[3]["movilidad"]}`
+    }
 })
